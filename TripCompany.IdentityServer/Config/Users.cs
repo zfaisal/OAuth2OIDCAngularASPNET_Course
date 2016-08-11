03539b4a599290b33170556ebaa5f196300fb9b4
zfaisal <zfaisal@pcr-net.net>
2016-08-11 17:10:57 -0400

    Update PCR: First unstable Commit

---
+++ TripCompany.IdentityServer/Config/Users.cs
@@ -0,0 +1,16 @@
+﻿using IdentityServer3.Core.Services.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TripCompany.IdentityServer.Config
+{
+    public static class Users
+    {
+        public static List<InMemoryUser> Get()
+        {
+            
+        }
+    }
+}
---
+++ TripCompany.IdentityServer/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TripCompany.IdentityServer.Startup))]
+
+namespace TripCompany.IdentityServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+        }
+    }
+}
