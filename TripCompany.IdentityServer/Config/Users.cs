7c54ed532a7065ee59edac5d547354c9b04b76dd
zfaisal <zfaisal@pcr-net.net>
2016-08-11 17:26:36 -0400

    Update PCR: Stable push

--- TripCompany.IdentityServer/Config/Users.cs
+++ TripCompany.IdentityServer/Config/Users.cs
@@ -10,7 +10,14 @@ namespace TripCompany.IdentityServer.Config
     {
         public static List<InMemoryUser> Get()
         {
-            
+            InMemoryUser m1 = new InMemoryUser { Username = "Kevin", Password="secret", Subject="wriwoper023424"};
+            InMemoryUser m2 = new InMemoryUser { Username = "Sven", Password = "secret", Subject = "jnghjdfdfg567889" };
+
+            List<InMemoryUser> inmemoUsers = new List<InMemoryUser>();
+            inmemoUsers.Add(m1);
+            inmemoUsers.Add(m2);
+            return inmemoUsers;
+
         }
     }
 }
